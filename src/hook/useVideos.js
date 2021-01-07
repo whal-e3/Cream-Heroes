8eaed691de5f503364b4f6e0cb647a7883ebdafb
eric8979 <buckelwal8979@khu.ac.kr>
2021-01-07 12:08:56 +0900

    Delete unused imports

--- src/hook/useVideos.js
+++ src/hook/useVideos.js
@@ -1,7 +1,5 @@
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 import youtube from '../api/youtube';
-import SearchBar from '../components/videoMenu/SearchBar';
 
 const useVideos = defaultSearchTerm => {
 	const [videos, setVideos] = useState([]);
