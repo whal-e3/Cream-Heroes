9c1b7d9aeeba5f9f3004f5c2f358c990f36f1039
eric8979 <buckelwal8979@khu.ac.kr>
2021-01-07 12:17:49 +0900

    Adding API KEYs to youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDEnBIVEXD-R1S46BYWBMSMNqMICAlWBXI';
+const KEY = 'AIzaSyDg04I10oF5vAt22OVVlFUtt78GTQ7AehY';
 
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
 	params: { part: 'snippet', maxResults: 10, key: KEY }
 });
+
+//AIzaSyB1YyJIC1EpKibrPliBAMZfcCZEpgRa-EU
